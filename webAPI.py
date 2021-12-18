d891599159463a3bddc071008ff6707409b6145c
siddhantmahesh <92044233+siddhantmahesh@users.noreply.github.com>
2021-12-18 10:52:01 +0000

    added post method for insert user

--- webAPI.py
+++ webAPI.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 import pymongo
+import requests
 
 app = Flask(__name__)
 
 client = pymongo.MongoClient("mongodb+srv://siddhant:b9cy-ca2@b9cyca2-database.oqc5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 database = client["ca2db"] #db instance
 exchangeRates = database["exchangeRates"] #collection instance
+
+# REF https://flask.palletsprojects.com/en/2.0.x/quickstart/
 
 @app.route('/getRates')
 def getRates ():
@@ -17,6 +20,12 @@ def getRates ():
         listData.append({item["currency"] : item["value"]}) #transform data for front end suitability
 
     return str(listData) #return as string/dict DOESNT return as list
+
+@app.route('/addUser', methods=['POST']) #add new user data
+def insertNewUser ():
+
+    data = request.get_json()
+    return str(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8080',debug=True) #add debug=true for dev purpose
